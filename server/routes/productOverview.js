7fe8eaf546e2d3f5f2dbe68c138da5e9dca97aa7
Peter Park <mac-main@Peters-MBP-2.lan>
2022-02-18 18:18:19 -0800

    Started setting up routers for DB interaction

--- server/routes/productOverview.js
+++ server/routes/productOverview.js
@@ -1,4 +1,9 @@
-const productOverviewRouter = require('express').Router();
+const router = require('express').Router();
+const proxy = require('express-http-proxy');
+
+const { productOverview } = require('../controllers/productOverview.js');
+
+router.route('/product')
 
 productOverviewRouter.get('/', (req, res) => {
   console.log('product');
@@ -6,3 +11,27 @@ productOverviewRouter.get('/', (req, res) => {
 })
 
 module.exports = productOverviewRouter;
+
+const { getQuestions, createQuestion, getAnswers, createAnswer, markQuestionHelpful, markQuestionReported, markAnswerHelpful, markAnswerReported } = questionsAndAnswers;
+
+router.route('/questions')
+  .get((req, res) => {proxy(getQuestions(req, res))})
+  .post((req, res) => {proxy(createQuestion(req, res))})
+
+router.route('/questions/:question_id/answers')
+  .get((req, res) => {proxy(getAnswers(req, res))})
+  .post((req, res) => {proxy(createAnswer(req, res))})
+
+router.route('/questions/:question_id/helpful')
+  .put((req, res) => {proxy(markQuestionHelpful(req, res))})
+
+router.route('/questions/:question_id/report')
+  .put((req, res) => {proxy(markQuestionReported(req, res))})
+
+router.route('/answers/:answer_id/helpful')
+  .put((req, res) => {proxy(markAnswerHelpful(req, res))})
+
+router.route('/answers/:answer_id/report')
+  .put((req, res) => {proxy(markAnswerReported(req, res))})
+
+module.exports = router;
