eafaa64640f67a13106c93902b4b67f00f6a1e26
Peter Park <mac-main@Peters-MBP-2.lan>
2022-02-23 16:54:38 -0800

    Restored ETL

---
+++ server/ETL/features.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const csv = require('fast-csv');
+const path = require('path');
+const { Product } = require('../db/product.js');
+
+const featuresStream = fs.createReadStream(path.join(__dirname, '/../../data/features.csv'));
+
+var featuresData = [];
+
+(async () => {
+  try {
+    featuresStream
+      .pipe(csv.parse({ headers: true, ignoreEmpty: true }))
+      .on('data', async row => {
+        try {
+          featuresData.push({
+            id: row['product_id'],
+            feature: row['feature'],
+            value: row['value']
+          });
+
+          if (featuresData.length === 500) {
+            for (var i = 0; i < featuresData.length; i++) {
+              await Product.updateOne({ id: featuresData[i].id }, { $addToSet: { features: { feature: featuresData[i].feature, value: featuresData[i].value }}});
+            }
+
+            console.log(`Inserted ${featuresData.length} features from Features CSV`);
+
+            featuresData = [];
+          }
+        }
+        catch (err) {
+          console.error(err);
+        }
+      })
+      .on('end', () => {
+        console.log('Imported Features CSV data');
+        currentProductId = 1;
+        process.exit();
+      })
+      .on('error', err => console.error(err));
+  }
+  catch (err) {
+    throw err;
+  }
+})().catch(err => console.error(err));
